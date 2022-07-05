03b24d774cea86e9336f94cb9c202c2b8ccc9e97
Johnathan Weil <weilj872@gmail.com>
2022-07-05 10:54:30 -0400

    deck player and dealer layouts js

--- blackjack.js
+++ blackjack.js
@@ -1,1 +1,17 @@
+//global obj
+const deck = {
+  cards: []
+}
 
+// player and computer dealer
+class Player {
+  constructor(name, hand) {}
+  hit() {}
+  stand() {}
+}
+class Dealer {
+  constructor(name, hand) {}
+  dealerHit() {}
+  dealerStand() {}
+  deal() {}
+}
