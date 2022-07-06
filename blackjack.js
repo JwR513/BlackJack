897d0c715501232a4b1f53fe1cb16195e3990d56
Johnathan Weil <weilj872@gmail.com>
2022-07-06 12:16:52 -0400

    more variables new button and new fnc

--- blackjack.js
+++ blackjack.js
@@ -1,4 +1,16 @@
+//Global
+const hitButton = document.querySelector('#hit')
+const standButton = document.querySelector('#stand')
+const dealButton = document.querySelector('#deal')
+// player and dealer
+const player = {
+  hand: []
+}
+const dealer = {
+  hand: []
+}
 //Deck creator
+// used https://devdojo.com/devdojo/create-a-deck-of-cards-in-javascript to learn how to create a dec of cards
 class Card {
   constructor(value, name, suit) {
     this.name = name
@@ -10,31 +22,28 @@ class Deck {
   constructor() {
     this.name = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
     this.suit = ['Diamonds', 'Spades', 'Hearts', 'Clubs']
+    //holds card objects in the deck
     let cards = []
-    for (let i = 0; i < this.suit.length; i++) {
+    //iterates through each suit and name and combines them into a new card aswell as giving them a value of their index +1 giving us 52 card variations
+    for (let s = 0; s < this.suit.length; s++) {
       for (let n = 0; n < this.name.length; n++) {
-        cards.push(new Card(n + 1, this.name[n], this.suit[i]))
+        cards.push(new Card(n + 1, this.name[n], this.suit[s]))
       }
     }
     return cards
   }
 }
-const myDeck = new Deck()
-console.log(myDeck)
+const gameDeck = new Deck()
 
-// player and computer dealer
-class Player {
-  constructor() {
-    this.hand = []
-  }
-  hit() {}
-  stand() {}
+console.log(gameDeck)
+// add cards from deck to player and dealer hands
+const dealCards = () => {
+  const randomCard = Math.floor(Math.random() * gameDeck.length)
+  dealer.hand.push(randomCard, randomCard)
 }
-class Dealer {
-  constructor(name, hand) {}
-  dealerHit() {}
-  dealerStand() {}
-  deal() {}
-}
-
-// testing
+console.log(dealer.hand)
+dealCards()
+//Event Handlers
+// dealButton.addEventListener('click')
+// hitButton.addEventListener('click')
+// standButton.addEventListener('click')
