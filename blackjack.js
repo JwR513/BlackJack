2adacc10475cd90d324de01653111cabb456e773
Johnathan Weil <weilj872@gmail.com>
2022-07-06 14:45:04 -0400

    changed player/dealer to arr and added if statment to for loop

--- blackjack.js
+++ blackjack.js
@@ -9,6 +9,7 @@ const player = {
 const dealer = {
   hand: []
 }
+let cards = []
 //Deck creator
 // used https://devdojo.com/devdojo/create-a-deck-of-cards-in-javascript to learn how to create a dec of cards
 class Card {
@@ -22,12 +23,14 @@ class Deck {
   constructor() {
     this.name = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
     this.suit = ['Diamonds', 'Spades', 'Hearts', 'Clubs']
-    //holds card objects in the deck
-    let cards = []
     //iterates through each suit and name and combines them into a new card aswell as giving them a value of their index +1 giving us 52 card variations
     for (let s = 0; s < this.suit.length; s++) {
       for (let n = 0; n < this.name.length; n++) {
-        cards.push(new Card(n + 1, this.name[n], this.suit[s]))
+        if (n <= 9) {
+          cards.push(new Card(n + 1, this.name[n], this.suit[s]))
+        } else {
+          cards.push(new Card(10, this.name[n], this.suit[s]))
+        }
       }
     }
     return cards
@@ -38,10 +41,10 @@ const gameDeck = new Deck()
 console.log(gameDeck)
 // add cards from deck to player and dealer hands
 const dealCards = () => {
-  const randomCard = Math.floor(Math.random() * gameDeck.length)
-  dealer.hand.push(randomCard, randomCard)
+  const randomCard1 = Math.floor(Math.random() * gameDeck.length)
+  const randomCard2 = Math.floor(Math.random() * gameDeck.length)
+  dealer.hand.push(randomCard1, randomCard2)
 }
-console.log(dealer.hand)
 dealCards()
 //Event Handlers
 // dealButton.addEventListener('click')
