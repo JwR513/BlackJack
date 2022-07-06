d363307bafe2c6d0c2ed041a66d09de40da03b47
Johnathan Weil <weilj872@gmail.com>
2022-07-05 21:41:33 -0400

    deck created

--- blackjack.js
+++ blackjack.js
@@ -1,15 +1,30 @@
-//global obj
-const deck = {
-  king: [1, 2, 3, 4],
-  queen: [1, 2, 3, 4],
-  jack: [1, 2, 3, 4],
-  ace: [1, 2, 3, 4],
-  regular: [2, 3, 4, 5, 6, 7, 8, 9, 10]
+//Deck creator
+class Card {
+  constructor(value, name, suit) {
+    this.name = name
+    this.suit = suit
+    this.value = value
+  }
 }
+class Deck {
+  constructor() {
+    this.name = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
+    this.suit = ['Diamonds', 'Spades', 'Hearts', 'Clubs']
+    let cards = []
+    for (let i = 0; i < this.suit.length; i++) {
+      for (let n = 0; n < this.name.length; n++) {
+        cards.push(new Card(n + 1, this.name[n], this.suit[i]))
+      }
+    }
+    return cards
+  }
+}
+const myDeck = new Deck()
+console.log(myDeck)
+
 // player and computer dealer
 class Player {
-  constructor(name) {
-    this.name = name
+  constructor() {
     this.hand = []
   }
   hit() {}
